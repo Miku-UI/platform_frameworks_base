e4c66aa9bd2270add5844b26f4e2143ddc2ffec0
maxwen <max.weninger@gmail.com>
2022-03-14 06:46:49 +0000

    Double tap to sleep on statusbar [1/2]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4077,6 +4077,9 @@ public class StatusBar extends SystemUI implements
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4084,6 +4087,9 @@ public class StatusBar extends SystemUI implements
             super.onChange(selfChange, uri);
             if (uri.equals(Settings.System.getUriFor(
                     Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN))) {
+                setLockscreenDoubleTapToSleep();
+ 	    } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE))) {
                 setLockscreenDoubleTapToSleep();
             }
         }
@@ -4094,8 +4100,8 @@ public class StatusBar extends SystemUI implements
     }
 
     private void setLockscreenDoubleTapToSleep() {
-        if (mNotificationPanelViewController != null) {
-            mNotificationPanelViewController.setLockscreenDoubleTapToSleep();
+        if (mNotificationShadeWindowViewController != null) {
+            mNotificationShadeWindowViewController.setLockscreenDoubleTapToSleep();
         }
     }
 
