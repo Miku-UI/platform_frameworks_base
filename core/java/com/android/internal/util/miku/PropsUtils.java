6c415e32ff8b87f98e19e9a4286ceb871a4b5c99
AmeChanRain <downloadbot007@gmail.com>
2022-06-07 12:10:45 +0000

    PropsUtils: Update raven fingerprint to Jun 2022

--- core/java/com/android/internal/util/miku/PropsUtils.java
+++ core/java/com/android/internal/util/miku/PropsUtils.java
@@ -59,7 +59,7 @@ public class PropsUtils {
         propsToChange.put("DEVICE", "raven");
         propsToChange.put("PRODUCT", "raven");
         propsToChange.put("MODEL", "Pixel 6 Pro");
-        propsToChange.put("FINGERPRINT", "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys");
+        propsToChange.put("FINGERPRINT", "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys");
 	propsToChangeMeizu = new HashMap<>();
 	propsToChangeMeizu.put("BRAND", "meizu");
 	propsToChangeMeizu.put("MANUFACTURER", "meizu");
